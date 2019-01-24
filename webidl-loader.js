10404a3402fc25d718b8b09de0a0932b4110e90b
Richard <Richard>
2019-01-24 22:41:03 +0000

    hello world

--- webidl-loader.js
+++ webidl-loader.js
@@ -1,14 +1,27 @@
-class WebSysLoader extends HTMLElement {
+class WebIDLLoader extends HTMLElement {
   connectedCallback(){
+    this.utf8dec = new TextDecoder ('utf-8')
     let wasmSrc = this.getAttribute("src");
     let exec = this.getAttribute("execute");
+    let memory = this.getAttribute("memory") || "memory";
     fetch(wasmSrc).then(response =>
       response.arrayBuffer()
     ).then(bytes =>
-      WebAssembly.instantiate(bytes, importObject)
+      WebAssembly.instantiate(bytes, {env:{
+        log:this.log.bind(this)
+      }})
     ).then(results => {
+      this.memory = results.instance.exports[memory];
       results.instance.exports[exec]();
     });
   }
+  log(start,len){
+    let str = this.readStringFromMemory(start,len);
+    console.log(str)
+  }
+  readStringFromMemory(start,len){
+    const view = new Uint8Array (this.memory.buffer, start, len)
+    return this.utf8dec.decode(view);
+  }
 }
-window.customElements.define('websys-loader', WebSysLoader);
+window.customElements.define('webidl-loader', WebIDLLoader);
