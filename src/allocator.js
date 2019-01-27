f9c75ebdc0c273df360045bff0d5db91510807ac
Richard <Richard>
2019-01-27 18:20:44 +0000

    making allocator useful

--- src/allocator.js
+++ src/allocator.js
@@ -4,17 +4,25 @@ export default function() {
     allocate:function(type,value){
       let a = allocations[type]
       if(!a){
-        a = allocations[type] = {values:[]}
+        a = allocations[type] = {values:[],empty:[]}
       }
       let i = a.values.length;
+      if(a.empty.length > 0) {
+        i = a.empty.pop();
+      }
       a.values[i] = value;
       return i;
     },
     release(type,handle){
-
+        allocations[type].values[handle] = undefined;
+        allocations[type].empty.push(handle);
     },
     get(type,handle){
-      return allocations[type].values[handle];
+      let ret =  allocations[type].values[handle];
+      if(!ret){
+        console.error(`Asked for ${type}:${handle} after it was released.`)
+      }
+      return ret;
     }
   }
 }
