b65b80b6eaf985c974d7e6df54826b34637ca529
Richard <Richard>
2019-03-17 05:11:53 +0000

    more minimal

--- examples/x-clock/src/lib.rs
+++ examples/x-clock/src/lib.rs
@@ -1,3 +1,6 @@
+use cstring::*;
+use callback::{add_callback,route_callback};
+
 extern "C" {
     fn console_log(msg: i32);
     fn global_getWindow() -> i32;
@@ -9,24 +12,7 @@ extern "C" {
     fn CustomElement_define(name: i32, attributes: i32);
 }
 
-fn cstr(s: &str) -> i32 {
-    std::ffi::CString::new(s).unwrap().into_raw() as i32
-}
-
-use std::collections::HashMap;
-
-static mut CALLBACKS: Option<HashMap<i32, Box<Fn(i32)>>> = None;
 static mut COMPONENTS: Option<Vec<XClock>> = None;
-
-#[no_mangle]
-pub fn callback(callback_id: i32, event: i32) {
-    unsafe {
-        let h = CALLBACKS.as_mut().unwrap().get(&callback_id);
-        if h.is_some() {
-            h.unwrap()(event);
-        }
-    }
-}
 
 struct XClock {
     element: i32,
@@ -70,29 +56,9 @@ impl XClock {
     }
 }
 
-fn listen<F>(element: i32, event_name: &str, f: Box<F>)
-where
-    F: Fn(i32) + 'static,
-{
-    unsafe {
-        let cb = global_createEventListener();
-        EventTarget_addEventListener(element, cstr(event_name), cb);
-        CALLBACKS.as_mut().unwrap().insert(cb, f);
-    }
-}
-
-fn setup_component(c: XClock) {
-    unsafe {
-        COMPONENTS.as_mut().unwrap().push(c);
-        let i = COMPONENTS.as_ref().unwrap().len() - 1;
-        COMPONENTS.as_ref().unwrap()[i].setup(i);
-    }
-}
-
 #[no_mangle]
 pub fn main() -> () {
     unsafe {
-        CALLBACKS = Some(HashMap::new());
         COMPONENTS = Some(Vec::new());
         let win = global_getWindow();
         listen(
@@ -101,9 +67,28 @@ pub fn main() -> () {
             Box::new(|event| {
                 let component_id = global_getProperty(event, cstr("detail"));
                 let c = XClock::new(component_id);
-                setup_component(c);
+                COMPONENTS.as_mut().unwrap().push(c);
+                let i = COMPONENTS.as_ref().unwrap().len() - 1;
+                COMPONENTS.as_ref().unwrap()[i].setup(i);
             }),
         );
         CustomElement_define(cstr("x-clock"), cstr("time"));
+    }
+}
+
+#[no_mangle]
+pub fn callback(callback_id: i32, event: i32) {
+    // this function routes callbacks to the right closure
+    route_callback(callback_id,event);
+}
+
+fn listen<F>(element: i32, event_name: &str, f: Box<F>)
+where
+    F: Fn(i32) + 'static,
+{
+    unsafe {
+        let cb = global_createEventListener();
+        EventTarget_addEventListener(element, cstr(event_name), cb);
+        add_callback(cb, f);
     }
 }
