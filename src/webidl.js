4683093c75bb83ec7d7ed4847043f2ae881040f7
Richard <Richard>
2019-03-17 23:16:40 +0000

    adding math and data

--- src/webidl.js
+++ src/webidl.js
@@ -31,6 +31,39 @@ function createWebIDLContext() {
       }
       return ALLOCATOR.a(p);
     },
+    Date_now: function() {
+      return Date.now();
+    },
+    Math_random: function() {
+      return Math.random();
+    },
+    Math_random_n: function(n) {
+      return Math.floor(Math.random() * n);
+    },
+    Math_cos: function(n) {
+      return Math.cos(n);
+    },
+    Math_sin: function(n) {
+      return Math.sin(n);
+    },
+    Math_tan: function(n) {
+      return Math.tan(n);
+    },
+    Math_acos: function(n) {
+      return Math.acos(n);
+    },
+    Math_asin: function(n) {
+      return Math.asin(n);
+    },
+    Math_atan: function(n) {
+      return Math.atan(n);
+    },
+    Math_atan2: function(n) {
+      return Math.atan2(n);
+    },
+    Math_random_n: function(n) {
+      return Math.floor(Math.random() * n);
+    },
     Element_attachShadow: function(instance) {
       let _instance = ALLOCATOR.g(instance);
       return ALLOCATOR.a(_instance.attachShadow({ mode: "open" }));
