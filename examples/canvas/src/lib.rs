58eb84a1eba5564d8ad0e7de60e7dc4b199d18f0
Richard <Richard>
2019-11-19 20:23:04 -0800

    updates

--- examples/canvas/src/lib.rs
+++ examples/canvas/src/lib.rs
@@ -2,53 +2,20 @@ use js_ffi::*;
 
 #[no_mangle]
 fn main() {
-	let api = globals::get::<API>().lock();
+    let screen = js!(document.querySelector).call_1(DOCUMENT, "#screen");
+    let ctx = js!(HTMLCanvasElement.prototype.getContext).call_1(screen, "2d");
 
-	let s = api.query_selector("#screen");
-	let ctx = api.get_context(s,"2d");
+    let fill_style = js!(function(color){
+        this.fillStyle = color;
+    });
+    let fill_rect = js!(CanvasRenderingContext2D.prototype.fillRect);
 
-	api.fill_style(ctx, "red");
-	api.fill_rect(ctx,0.0,0.0,50.0,50.0);
+    fill_style.call_1(ctx, "red");
+    fill_rect.call_4(ctx, 0.0, 0.0, 50.0, 50.0);
 
-	api.fill_style(ctx, "green");
-	api.fill_rect(ctx,15.0,15.0,50.0,50.0);
+    fill_style.call_1(ctx, "green");
+    fill_rect.call_4(ctx, 15.0, 15.0, 50.0, 50.0);
 
-	api.fill_style(ctx, "blue");
-	api.fill_rect(ctx,30.0,30.0,50.0,50.0);
-}
-
-struct API {
-	query_selector_handle: JSValue,
-	get_context_handle: JSValue,
-	fill_style_handle: JSValue,
-	fill_rect_handle: JSValue,
-}
-
-impl Default for API {
-	fn default() -> Self {
-		API {
-			query_selector_handle: register("document.querySelector"),
-			get_context_handle: register("HTMLCanvasElement.prototype.getContext"),
-			fill_style_handle: register("(function(s){this.fillStyle = s;})"),
-			fill_rect_handle: register("CanvasRenderingContext2D.prototype.fillRect"),
-		}
-	}
-}
-
-impl API {
-	fn query_selector(&self, s: &str) -> JSValue {
-		call_1(DOCUMENT, self.query_selector_handle,TYPE_STRING,to_js_string(s))
-	}
-
-	fn get_context(&self, o: JSValue, s: &str) -> JSValue {
-		call_1(o,self.get_context_handle,TYPE_STRING,to_js_string(s))
-	}
-
-	fn fill_style(&self, o: JSValue, s: &str) {
-		call_1(o,self.fill_style_handle,TYPE_STRING,to_js_string(s));
-	}
-
-	fn fill_rect(&self, o: JSValue, x: JSValue, y: JSValue, w: JSValue, h: JSValue) {
-		call_4(o,self.fill_rect_handle,TYPE_NUM,x,TYPE_NUM,y,TYPE_NUM,w,TYPE_NUM,h);
-	}
+    fill_style.call_1(ctx, "blue");
+    fill_rect.call_4(ctx, 30.0, 30.0, 50.0, 50.0);
 }
--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -2,24 +2,5 @@ use js_ffi::*;
 
 #[no_mangle]
 pub fn main() -> () {
-    let console = globals::get::<Console>().lock();
-    console.log("Hello world!")
-}
-
-struct Console {
-    fn_log:JSValue
-}
-
-impl Default for Console {
-    fn default() -> Self {
-        Console {
-            fn_log:register("console.log")
-        }
-    }
-}
-
-impl Console {
-    fn log(&self,msg:&str){
-        call_1(UNDEFINED,self.fn_log,TYPE_STRING,to_js_string(msg));
-    }
+    js!(console.log).invoke_1("Hello World");
 }
